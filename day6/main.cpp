5ff6fdefa8d735fd6e0fedc004e72b150b28891c
Adrian Scripca <benishor@gmail.com>
2021-12-06 12:18:31 +0200

    Day 6: an exercise in optimisation

--- day6/main.cpp
+++ day6/main.cpp
@@ -1,45 +1,82 @@
 #include <numeric>
 #include "solution.h"
 
-using fish_map = std::map<int, uint64_t>;
+static unsigned long compute(std::ostream& out, const std::vector<int>& numbers) {
+    auto start = std::chrono::steady_clock::now();
+
+    size_t data[18]{};
+    size_t *fish_timers, *new_timers;
+    fish_timers = &data[0];
+    new_timers = &data[9];
+
+    for (auto& n: numbers) {
+        fish_timers[n]++;
+    }
+
+    for (int day = 0; day < 80; day++) {
+        new_timers[0] = new_timers[1] = new_timers[2] = new_timers[3] = new_timers[4] = new_timers[5] = new_timers[6] = new_timers[7] = new_timers[8] = 0;
+        for (int t = 0; t < 9; t++) {
+            uint64_t count = fish_timers[t];
+            if (t == 0) {
+                new_timers[6] += count;
+                new_timers[8] = count;
+            } else {
+                new_timers[t - 1] += count;
+            }
+        }
+
+        std::swap(fish_timers, new_timers);
+    }
+    out << fish_timers[0] +
+           fish_timers[1] +
+           fish_timers[2] +
+           fish_timers[3] +
+           fish_timers[4] +
+           fish_timers[5] +
+           fish_timers[6] +
+           fish_timers[7] +
+           fish_timers[8] << std::endl;
+
+    for (int day = 80; day < 256; day++) {
+        new_timers[0] = new_timers[1] = new_timers[2] = new_timers[3] = new_timers[4] = new_timers[5] = new_timers[6] = new_timers[7] = new_timers[8] = 0;
+        for (int t = 0; t < 9; t++) {
+            uint64_t count = fish_timers[t];
+            if (t == 0) {
+                new_timers[6] += count;
+                new_timers[8] = count;
+            } else {
+                new_timers[t - 1] += count;
+            }
+        }
+
+        std::swap(fish_timers, new_timers);
+    }
+
+    out << fish_timers[0] +
+           fish_timers[1] +
+           fish_timers[2] +
+           fish_timers[3] +
+           fish_timers[4] +
+           fish_timers[5] +
+           fish_timers[6] +
+           fish_timers[7] +
+           fish_timers[8] << std::endl;
+
+    auto end = std::chrono::steady_clock::now();
+    auto ns_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    return ns_taken.count();
+}
 
 class day6 : public aoc::solution {
 protected:
     void run(std::istream& in, std::ostream& out) override {
-        fish_map fish_timers;
         auto numbers = aoc::csv_elements_from_stream<int>(in);
-        for (auto& n: numbers) {
-            fish_timers[n]++;
-        }
 
-        for (int day = 0; day < 256; day++) {
-            fish_map new_timers;
-            for (int t = 0; t <= 8; t++) {
-                uint64_t count = fish_timers[t];
-                if (t == 0) {
-                    new_timers[6] += count;
-                    new_timers[8] = count;
-                } else {
-                    new_timers[t - 1] += count;
-                }
-            }
-            fish_timers = new_timers;
-            if (day == 79) {
-                out << std::accumulate(std::begin(fish_timers),
-                                       std::end(fish_timers),
-                                       0ULL,
-                                       [](uint64_t value, const std::map<int, uint64_t>::value_type& p) -> uint64_t {
-                                           return value + p.second;
-                                       }) << std::endl;
-            }
+        unsigned long total_time{0};
+        for (int i = 0; i < 100; i++) {
+            total_time += compute(out, numbers);
         }
-
-        out << std::accumulate(std::begin(fish_timers),
-                               std::end(fish_timers),
-                               0ULL,
-                               [](uint64_t value, const std::map<int, uint64_t>::value_type& p) -> uint64_t {
-                                   return value + p.second;
-                               }) << std::endl;
+        std::cout << "ns taken: " << total_time / 100 << std::endl;
     }
 };
 
