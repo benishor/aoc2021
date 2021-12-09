910e6e142b6905098fff7c32d2138832c6a83e91
Adrian Scripca <benishor@gmail.com>
2021-12-09 08:45:37 +0200

    Day 9: reorder things arround

--- day9/main.cpp
+++ day9/main.cpp
@@ -2,10 +2,34 @@
 
 class day9 : public aoc::solution {
 protected:
-
     void run(std::istream& in, std::ostream& out) override {
-        std::array<std::array<uint8_t, 100>, 100> heightmap{};
 
+        auto heightmap = parse_data(in);
+
+        uint64_t part1{0};
+        std::vector<uint32_t> basins{};
+
+        for (size_t y = 0; y < 100; y++) {
+            for (size_t x = 0; x < 100; x++) {
+                if (is_local_minimum(heightmap, x, y)) {
+                    part1 += 1 + heightmap[y][x];
+
+                    flood_fill_basin(heightmap, x, y);
+                    basins.push_back(count_flagged(heightmap));
+                    clear_flags(heightmap);
+                }
+            }
+        }
+        out << part1 << std::endl;
+
+        std::sort(std::rbegin(basins), std::rend(basins));
+        uint64_t part2 = basins[0] * basins[1] * basins[2];
+        out << part2 << std::endl;
+    }
+
+
+    static std::array<std::array<uint8_t, 100>, 100> parse_data(std::istream& in) {
+        std::array<std::array<uint8_t, 100>, 100> heightmap{};
         size_t l{0};
         for (std::string line; std::getline(in, line);) {
             size_t i{0};
@@ -15,24 +39,7 @@ protected:
             }
             l++;
         }
-
-        uint64_t part1{0};
-        std::vector<uint32_t> basins{};
-        for (size_t y = 0; y < 100; y++) {
-            for (size_t x = 0; x < 100; x++) {
-                if (is_local_minimum(heightmap, x, y)) {
-                    part1 += 1 + heightmap[y][x];
-                    clear_heightmap_flags(heightmap);
-                    flag_basin(heightmap, x, y);
-                    basins.push_back(count_flagged(heightmap));
-                }
-            }
-        }
-        out << part1 << std::endl;
-
-        std::sort(std::rbegin(basins), std::rend(basins));
-        uint64_t part2 = basins[0] * basins[1] * basins[2];
-        out << part2 << std::endl;
+        return heightmap;
     }
 
     static bool is_local_minimum(std::array<std::array<uint8_t, 100>, 100>& heightmap, size_t x, size_t y) {
@@ -44,7 +51,7 @@ protected:
         return true;
     }
 
-    static void clear_heightmap_flags(std::array<std::array<uint8_t, 100>, 100>& heightmap) {
+    static void clear_flags(std::array<std::array<uint8_t, 100>, 100>& heightmap) {
         for (size_t y = 0; y < 100; y++) {
             for (size_t x = 0; x < 100; x++) {
                 heightmap[y][x] &= 0x7f;
@@ -52,20 +59,20 @@ protected:
         }
     }
 
-    static void flag_basin(std::array<std::array<uint8_t, 100>, 100>& heightmap, size_t x, size_t y) {
+    static void flood_fill_basin(std::array<std::array<uint8_t, 100>, 100>& heightmap, size_t x, size_t y) {
         if (heightmap[y][x] < 9) {
             heightmap[y][x] |= 0x80;
             if (x > 0) {
-                flag_basin(heightmap, x - 1, y);
+                flood_fill_basin(heightmap, x - 1, y);
             }
             if (x < 99) {
-                flag_basin(heightmap, x + 1, y);
+                flood_fill_basin(heightmap, x + 1, y);
             }
             if (y > 0) {
-                flag_basin(heightmap, x, y - 1);
+                flood_fill_basin(heightmap, x, y - 1);
             }
             if (y < 99) {
-                flag_basin(heightmap, x, y + 1);
+                flood_fill_basin(heightmap, x, y + 1);
             }
         }
     }
